f98f8ffa220b31f2c9c5c37d9ee4db8ae6b2e7a7
TongaDM <tongadm@gmail.com>
2024-01-07 10:23:17 -0300

    starting

--- DOM/teclado.js
+++ DOM/teclado.js
@@ -1,4 +1,22 @@
 export function shortCuts(e) {
- console.log(e.type);
- console.log(e);
-}
+    console.log('Tipo de evento:', e.type);
+    console.log('Tecla:', e.key);
+    console.log('Código de tecla:', e.keyCode);
+    console.log('ctrl:', e.ctrlKey);
+    console.log('alt:', e.altKey);
+    console.log('shift:', e.shiftKey);
+    console.log(e);
+
+    if (e.key === "a" && e.altKey) {
+        alert("haz lanzado una alerta con el teclado")
+    }
+
+    if (e.key === "c" && e.altKey) {
+        confirm("haz lanzado una Confirmacion con el teclado")
+    }
+
+    if (e.key === "p" && e.altKey) {
+        alert("haz lanzado un Prompt con el teclado")
+    }
+  }
+
