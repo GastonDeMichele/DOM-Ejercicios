78f5aa44fd5c790d5890be740c195904c88f0f51
TongaDM <tongadm@gmail.com>
2024-01-16 07:27:55 -0300

    starting

--- DOM/boton_scroll.js
+++ DOM/boton_scroll.js
@@ -3,4 +3,9 @@ w = window;
 
 export default function scrollTopButton(btn){
     const $scrollBtn = d.querySelector(btn)
+
+    w.addEventListener("scroll", e =>{
+        
+    })
+    d.addEventListener("click", e =>{})
 }
