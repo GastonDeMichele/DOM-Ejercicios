7cfa5ad10c6e52e6e5b7219dea07988ab2d66b16
TongaDM <tongadm@gmail.com>
2024-01-16 06:56:01 -0300

    starting

--- DOM/cuenta_regresiva.js
+++ DOM/cuenta_regresiva.js
@@ -1,4 +1,27 @@
-const d = document
-export default function countdown(){
-  
+const d = document;
+export default function countdown(id, limitDate, finalMessage) {
+    const $countDown = d.getElementById(id),
+    countdownDate = new Date(limitDate).getTime()
+
+    let countDownTempo = setInterval(() => {
+        let now = new Date().getTime(),
+            limitTime = countdownDate - now,
+            days = Math.floor(limitTime / (1000 * 60 * 60 * 24)),
+            hours = (
+                "0" + Math.floor((limitTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+                )).slice(-2),
+            minutes = (
+                "0" + Math.floor((limitTime % (1000 * 60 * 60)) / (1000 * 60)
+                )).slice(-2),
+            seconds = (
+                "0" + Math.floor((limitTime % (1000 * 60)) / (1000))).slice(-2);
+
+        $countDown.innerHTML = `<h3>faltan ${days} días ${hours} horas ${minutes} minutos ${seconds} segundos</h3>`
+
+        if (limitTime < 0) {
+            clearInterval(countDownTempo);
+            $countDown.innerHTML = `<h3>${finalMessage}</h3>`
+        }
+
+    }, 1000)
 }
