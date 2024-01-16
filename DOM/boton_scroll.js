385f2b996952b5346de2cf438f32e059f01852b4
Gaston De Michele <108197445+GastonDeMichele@users.noreply.github.com>
2024-01-16 11:18:42 -0300

    starting

--- DOM/boton_scroll.js
+++ DOM/boton_scroll.js
@@ -5,7 +5,15 @@ export default function scrollTopButton(btn){
     const $scrollBtn = d.querySelector(btn)
 
     w.addEventListener("scroll", e =>{
+     let scrollTop = w.pageYOffset || d.documentElement.scrollTop
+     
+     if (condition) {
         
+     }
+     console.log(w.pageYOffset, d.documentElement.scrollTop)
     })
-    d.addEventListener("click", e =>{})
+
+    d.addEventListener("click", e =>{
+
+    })
 }
