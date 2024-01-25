73612e5d8a35e0b362908b84266de95ce5341dd8
Gaston De Michele <108197445+GastonDeMichele@users.noreply.github.com>
2024-01-25 20:47:05 -0300

    starting

--- DOM/deteccion_dispositivos.js
+++ DOM/deteccion_dispositivos.js
@@ -3,5 +3,12 @@ n = navigator,
 ua = n.userAgent
 
 export default function userDeviceInfo(id){
+const $id = d.getElementById(id),
+isMobile = {
+    android:()=>ua.match(/android/i),
+    ios: ()=>ua.match(/iphone/)
+},
+isDesktop = {},
+isBrowser = {};
 
 }
