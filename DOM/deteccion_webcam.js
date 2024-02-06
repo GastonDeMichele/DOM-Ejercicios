4f7e270c740bd130001cfc5a3979e9ac685f248f
TongaDM <tongadm@gmail.com>
2024-02-06 11:39:00 -0300

    starting

--- DOM/deteccion_webcam.js
+++ DOM/deteccion_webcam.js
@@ -2,5 +2,18 @@ const d = document,
 n = navigator
 export default function webCam(id){
 const $videos = d.getElementById(id);
-console.log(n.mediaDevices)
+//console.log(n.mediaDevices.getUserMedia)
+
+if (n.mediaDevices.getUserMedia) {
+  n.mediaDevices.getUserMedia({video:true,audio:false})
+  .then((stream)=>{
+    console.log(stream)
+    $videos.scrObject = stream
+    $videos.play()
+  })
+  .catch((err)=>{
+    $videos.insertAdjacentHTML("afterend", `<p><mark>${err}<mark/><p/>`)
+    console.log(`sucedio el siguiente error: ${err}`)
+  })  
+}
 }
