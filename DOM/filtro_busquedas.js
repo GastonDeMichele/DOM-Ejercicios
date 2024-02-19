25beba11da2613e613579d01e425f29963022baf
TongaDM <tongadm@gmail.com>
2024-02-19 15:51:40 -0300

    fresh

--- DOM/filtro_busquedas.js
+++ DOM/filtro_busquedas.js
@@ -5,9 +5,10 @@ d.addEventListener("keyup", (e)=>{
     if (e.target.matches(input)) {
         //console.log(key)
         //console.log(e.target.value)
+        const valueInput = e.target.value.toLowerCase();
 
         d.querySelectorAll(selector).forEach((el)=>
-         el.textContent.toLowerCase().includes(e.target.value)
+         el.textContent.toLowerCase().includes(valueInput)
          ?el.classList.remove("filter")
          :el.classList.add("filter")
         )
